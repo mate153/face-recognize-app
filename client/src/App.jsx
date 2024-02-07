9be4a1eb314fe2a4b8ca60d2f379754f7239b8e4
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2024-02-07 13:30:10 +0100

    [ ADD ] React vite skeleton created with bootstrap

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Container } from 'react-bootstrap';
+import './App.css'
+
+function App() {
+
+  return (
+    <Container className='bg-dark mt-auto' fluid style={{ padding: 0, margin: 0, width: '100%' }}>
+      
+    </Container>
+  )
+}
+
+export default App
