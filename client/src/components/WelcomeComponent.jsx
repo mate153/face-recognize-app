a2158cd76c89d9b0229c32768eb8ae6835b0c826
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2024-02-10 11:44:56 +0100

    [ ADD ] Welcome component created

---
+++ client/src/components/WelcomeComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function WelcomeComponent() {
+  return (
+    <>
+      <div>WelcomeComponent</div>
+      <button href='login-comonent'>Quit</button>
+    </>
+  )
+}
+
+export default WelcomeComponent
