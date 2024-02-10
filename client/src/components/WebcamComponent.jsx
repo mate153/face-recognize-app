048bd1b4379a5e33c734971a67508220f80fe019
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2024-02-10 11:44:32 +0100

    [ UPDATE ] Fun's -> fun component / detect face descriptor / test fetch to login added

--- client/src/components/WebcamComponent.jsx
+++ client/src/components/WebcamComponent.jsx
@@ -1,194 +1,248 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as faceapi from '../../dist/face-api.esm.js';
 import './style/WebcamComponent.css';
 import { Container } from 'react-bootstrap';
 
-const modelPath = '../../model/';
-const minScore = 0.2;
-const maxResults = 5;
-let optionsSSDMobileNet;
-
-function str(json) {
-  let text = '<font color="lightblue">';
-  text += json ? JSON.stringify(json).replace(/{|}|"|\[|\]/g, '').replace(/,/g, ', ') : '';
-  text += '</font>';
-  return text;
-}
-
-function log(...txt) {
-  console.log(...txt);
-  const div = document.getElementById('log');
-  if (div) div.innerHTML += `<br>${txt}`;
-}
-
-function drawFaces(canvas, data, fps) {
-  const ctx = canvas.getContext('2d', { willReadFrequently: true });
-  if (!ctx) return;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.font = 'small-caps 20px "Segoe UI"';
-  ctx.fillStyle = 'white';
-  ctx.fillText(`FPS: ${fps}`, 10, 25);
-  for (const person of data) {
-    // draw box around each face
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = 'deepskyblue';
-    ctx.fillStyle = 'deepskyblue';
-    ctx.globalAlpha = 0.6;
-    ctx.beginPath();
-    ctx.rect(person.detection.box.x, person.detection.box.y, person.detection.box.width, person.detection.box.height);
-    ctx.stroke();
-    ctx.globalAlpha = 1;
-    // draw text labels
-    const expression = Object.entries(person.expressions).sort((a, b) => b[1] - a[1]);
-    ctx.fillStyle = 'black';
-    ctx.fillText(`gender: ${Math.round(100 * person.genderProbability)}% ${person.gender}`, person.detection.box.x, person.detection.box.y - 59);
-    ctx.fillText(`expression: ${Math.round(100 * expression[0][1])}% ${expression[0][0]}`, person.detection.box.x, person.detection.box.y - 41);
-    ctx.fillText(`age: ${Math.round(person.age)} years`, person.detection.box.x, person.detection.box.y - 23);
-    ctx.fillText(`roll:${person.angle.roll}° pitch:${person.angle.pitch}° yaw:${person.angle.yaw}°`, person.detection.box.x, person.detection.box.y - 5);
-    ctx.fillStyle = 'lightblue';
-    ctx.fillText(`gender: ${Math.round(100 * person.genderProbability)}% ${person.gender}`, person.detection.box.x, person.detection.box.y - 60);
-    ctx.fillText(`expression: ${Math.round(100 * expression[0][1])}% ${expression[0][0]}`, person.detection.box.x, person.detection.box.y - 42);
-    ctx.fillText(`age: ${Math.round(person.age)} years`, person.detection.box.x, person.detection.box.y - 24);
-    ctx.fillText(`roll:${person.angle.roll}° pitch:${person.angle.pitch}° yaw:${person.angle.yaw}°`, person.detection.box.x, person.detection.box.y - 6);
-    // draw face points for each face
-    ctx.globalAlpha = 0.8;
-    ctx.fillStyle = 'lightblue';
-    const pointSize = 2;
-    for (let i = 0; i < person.landmarks.positions.length; i++) {
-      ctx.beginPath();
-      ctx.arc(person.landmarks.positions[i].x, person.landmarks.positions[i].y, pointSize, 0, 2 * Math.PI);
-      ctx.fill();
-    }
-  }
-}
-
-async function detectVideo(video, canvas) {
-  if (!video || video.paused) return false;
-  const t0 = performance.now();
-  faceapi
-    .detectAllFaces(video, optionsSSDMobileNet)
-    .withFaceLandmarks()
-    .withFaceExpressions()
-    // .withFaceDescriptors()
-    .withAgeAndGender()
-    .then((result) => {
-      const fps = 1000 / (performance.now() - t0);
-      drawFaces(canvas, result, fps.toLocaleString());
-      requestAnimationFrame(() => detectVideo(video, canvas));    
-      return true;
-    })
-    .catch((err) => {
-      log(`Detect Error: ${str(err)}`);
-      return false;
-    });
-  return false;
-}
-
-async function setupCamera() {
-  const video = document.getElementById('video');
-  const canvas = document.getElementById('canvas');
-  if (!video || !canvas) return null;
-
-  //log('Setting up camera');
-  if (!navigator.mediaDevices) {
-    log('Camera Error: access not supported');
-    return null;
-  }
-  let stream;
-  const constraints = { audio: false, video: { facingMode: 'user', resizeMode: 'crop-and-scale' } };
-  if (window.innerWidth > window.innerHeight) constraints.video.width = { ideal: window.innerWidth };
-  else constraints.video.height = { ideal: window.innerHeight };
-  try {
-    stream = await navigator.mediaDevices.getUserMedia(constraints);
-  } catch (err) {
-    if (err.name === 'PermissionDeniedError' || err.name === 'NotAllowedError') log(`Camera Error: camera permission denied: ${err.message || err}`);
-    if (err.name === 'SourceUnavailableError') log(`Camera Error: camera not available: ${err.message || err}`);
-    return null;
-  }
-  if (stream) {
-    video.srcObject = stream;
-  } else {
-    log('Camera Error: stream empty');
-    return null;
-  }
-  const track = stream.getVideoTracks()[0];
-  const settings = track.getSettings();
-  if (settings.deviceId) delete settings.deviceId;
-  if (settings.groupId) delete settings.groupId;
-  if (settings.aspectRatio) settings.aspectRatio = Math.trunc(100 * settings.aspectRatio) / 100;
-
-  //log(`Camera active: ${track.label}`);
-  //log(`Camera settings: ${str(settings)}`);
-  canvas.addEventListener('click', () => {
-    if (video && video.readyState >= 2) {
-      if (video.paused) {
-        video.play();
-        detectVideo(video, canvas);
-      } else {
-        video.pause();
-      }
-    }
-    //log(`Camera state: ${video.paused ? 'paused' : 'playing'}`);
-  });
-  return new Promise((resolve) => {
-    video.onloadeddata = async () => {
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-      video.play();
-      detectVideo(video, canvas);
-      resolve(true);
-    };
-  });
-}
-
-async function setupFaceAPI() {
-  // log('Models loading');
-  // await faceapi.nets.tinyFaceDetector.load(modelPath); // using ssdMobilenetv1
-
-  await faceapi.nets.ssdMobilenetv1.load(modelPath);
-  await faceapi.nets.ageGenderNet.load(modelPath);
-  await faceapi.nets.faceLandmark68Net.load(modelPath);
-  await faceapi.nets.faceRecognitionNet.load(modelPath);
-  await faceapi.nets.faceExpressionNet.load(modelPath);
-  optionsSSDMobileNet = new faceapi.SsdMobilenetv1Options({ minConfidence: minScore, maxResults });
-
-  //log(`Models loaded: ${str(faceapi.tf.engine().state.numTensors)} tensors`);
-}
-
-async function main() {
-  //log('FaceAPI WebCam Test');
-
-  await faceapi.tf.setBackend('webgl');
-  await faceapi.tf.ready();
-
-  if (faceapi.tf?.env().flagRegistry.CANVAS2D_WILL_READ_FREQUENTLY) faceapi.tf.env().set('CANVAS2D_WILL_READ_FREQUENTLY', true);
-  if (faceapi.tf?.env().flagRegistry.WEBGL_EXP_CONV) faceapi.tf.env().set('WEBGL_EXP_CONV', true);
-  if (faceapi.tf?.env().flagRegistry.WEBGL_EXP_CONV) faceapi.tf.env().set('WEBGL_EXP_CONV', true);
-
-  //log(`Version: FaceAPI ${str(faceapi?.version || '(not loaded)')} TensorFlow/JS ${str(faceapi.tf?.version_core || '(not loaded)')} Backend: ${str(faceapi.tf?.getBackend() || '(not loaded)')}`);
-
-  await setupFaceAPI();
-  await setupCamera();
-}
-
-function Webcam() {
+function Webcam({register, setLoginTrue, login}) {
+  const modelPath = '../../model/';
+  const minScore = 0.2;
+  const maxResults = 5;
+  let optionsSSDMobileNet;
+  const [isConditionMet, setIsConditionMet] = useState(false);
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
   const logRef = useRef(null);
+  const [reference, setReference] = useState(false);
+  let test = [];
 
   useEffect(() => {
     main();
   }, []);
 
+  const borderStyle = {
+    border: isConditionMet ? '20px solid green' : '20px solid red'
+  }
+
+  function drawFaces(canvas, data, fps, video) {
+    const ctx = canvas.getContext('2d', { willReadFrequently: true });
+    if (!ctx) return;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.font = 'small-caps 20px "Segoe UI"';
+    ctx.fillStyle = 'white';
+    ctx.fillText(`FPS: ${fps}`, 10, 25);
+    for (const person of data) {
+      // draw box around each face
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = 'deepskyblue';
+      ctx.fillStyle = 'deepskyblue';
+      ctx.globalAlpha = 0.6;
+      ctx.beginPath();
+      ctx.rect(person.detection.box.x, person.detection.box.y, person.detection.box.width, person.detection.box.height);
+      ctx.stroke();
+      ctx.globalAlpha = 1;
+      // draw text labels
+      const expression = Object.entries(person.expressions).sort((a, b) => b[1] - a[1]);
+      if(person.angle.yaw > -20 && person.angle.yaw < 20 && person.angle.pitch < 10 && person.angle.pitch > -2){
+        setIsConditionMet(true)
+        detectFaceDescriptor(video)
+      } else {
+        setIsConditionMet(false)
+      }
+      ctx.fillStyle = 'black';
+      ctx.fillText(`gender: ${Math.round(100 * person.genderProbability)}% ${person.gender}`, person.detection.box.x, person.detection.box.y - 59);
+      ctx.fillText(`expression: ${Math.round(100 * expression[0][1])}% ${expression[0][0]}`, person.detection.box.x, person.detection.box.y - 41);
+      ctx.fillText(`age: ${Math.round(person.age)} years`, person.detection.box.x, person.detection.box.y - 23);
+      ctx.fillText(`roll:${person.angle.roll}° pitch:${person.angle.pitch}° yaw:${person.angle.yaw}°`, person.detection.box.x, person.detection.box.y - 5);
+      ctx.fillStyle = 'lightblue';
+      ctx.fillText(`gender: ${Math.round(100 * person.genderProbability)}% ${person.gender}`, person.detection.box.x, person.detection.box.y - 60);
+      ctx.fillText(`expression: ${Math.round(100 * expression[0][1])}% ${expression[0][0]}`, person.detection.box.x, person.detection.box.y - 42);
+      ctx.fillText(`age: ${Math.round(person.age)} years`, person.detection.box.x, person.detection.box.y - 24);
+      ctx.fillText(`roll:${person.angle.roll}° pitch:${person.angle.pitch}° yaw:${person.angle.yaw}°`, person.detection.box.x, person.detection.box.y - 6);
+      // draw face points for each face
+      ctx.globalAlpha = 0.8;
+      ctx.fillStyle = 'lightblue';
+      const pointSize = 2;
+      for (let i = 0; i < person.landmarks.positions.length; i++) {
+        ctx.beginPath();
+        ctx.arc(person.landmarks.positions[i].x, person.landmarks.positions[i].y, pointSize, 0, 2 * Math.PI);
+        ctx.fill();
+      }
+    }
+  }
+
+  async function detectVideo(video, canvas) {
+    if (!video || video.paused) return false;
+    const t0 = performance.now();
+    try {
+      const result = await faceapi
+        .detectAllFaces(video)
+        .withFaceLandmarks()
+        .withFaceExpressions()
+        .withAgeAndGender();      
+      const fps = 1000 / (performance.now() - t0);
+      drawFaces(canvas, result, fps.toLocaleString(), video);
+      requestAnimationFrame(() => detectVideo(video, canvas));
+      return true;
+    } catch (err) {
+      console.log(`Detect Error: ${String(err)}`);
+      return false;
+    }
+  }
+
+  async function sendDescriptorToServer(descriptor) {
+    try {
+      const res = await fetch('/api/sendDescriptor', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ descriptor })
+      });
+  
+      if (!res.ok) {
+        throw new Error('Failed to send descriptor to the server');
+      }  
+      console.log('Descriptor sent successfully');
+    } catch (err) {
+      console.log(`Fetch Error: ${err}`);
+    }
+  }
+
+  async function detectFaceDescriptor(video) {
+    if (!video || video.paused) return false;
+    if(!reference){
+      try {
+        const singleResult = await faceapi
+          .detectSingleFace(video)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+
+        const descriptor = singleResult.descriptor;
+
+        if(test.length < 2) test.push(descriptor)
+
+        if (descriptor != null && register && test.length == 1) {
+          await sendDescriptorToServer(test);
+          setLoginTrue(true);
+        }
+
+        if(login) fetchTmpData(singleResult);
+
+        return true;
+      } catch (err) {
+        console.log(`Detect Error: ${String(err)}`);
+        return false;
+      }
+    }
+  }
+
+  async function fetchTmpData(singleResult){
+    console.log("here");
+    setReference(true);
+    try {
+      const data = await fetch('/api/getDescriptor')
+      const res = await data.json()
+
+      const propertyValues = Object.values(res[0]);
+      console.log(propertyValues);
+      const faceMatcher = new faceapi.FaceMatcher(singleResult)
+      const bestMatch = faceMatcher.findBestMatch(propertyValues)
+      console.log(bestMatch)  
+      console.log("hurra");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function setupCamera() {
+    const video = document.getElementById('video');
+    const canvas = document.getElementById('canvas');
+    if (!video || !canvas) return null;
+
+    if (!navigator.mediaDevices) {
+      console.log('Camera Error: access not supported');
+      return null;
+    }
+
+    let stream;
+    const constraints = { audio: false, video: { facingMode: 'user', resizeMode: 'crop-and-scale' } };
+
+    if (window.innerWidth > window.innerHeight) constraints.video.width = { ideal: window.innerWidth };
+    else constraints.video.height = { ideal: window.innerHeight };
+
+    try {
+      stream = await navigator.mediaDevices.getUserMedia(constraints);
+    } catch (err) {
+      if (err.name === 'PermissionDeniedError' || err.name === 'NotAllowedError') log(`Camera Error: camera permission denied: ${err.message || err}`);
+      if (err.name === 'SourceUnavailableError') log(`Camera Error: camera not available: ${err.message || err}`);
+      return null;
+    }
+
+    if (stream) {
+      video.srcObject = stream;
+    } else {
+      console.log('Camera Error: stream empty');
+      return null;
+    }
+
+    const track = stream.getVideoTracks()[0];
+    const settings = track.getSettings();
+
+    if (settings.deviceId) delete settings.deviceId;
+    if (settings.groupId) delete settings.groupId;
+    if (settings.aspectRatio) settings.aspectRatio = Math.trunc(100 * settings.aspectRatio) / 100;
+
+    canvas.addEventListener('click', () => {
+      if (video && video.readyState >= 2) {
+        if (video.paused) {
+          video.play();
+          detectVideo(video, canvas);
+        } else {
+          video.pause();
+        }
+      }
+    });
+
+    return new Promise((resolve) => {
+      video.onloadeddata = async () => {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        video.play();
+        detectVideo(video, canvas);
+        resolve(true);
+      };
+    });
+  }  
+
+  async function setupFaceAPI() {
+    await faceapi.nets.ssdMobilenetv1.load(modelPath);
+    await faceapi.nets.ageGenderNet.load(modelPath);
+    await faceapi.nets.faceLandmark68Net.load(modelPath);
+    await faceapi.nets.faceRecognitionNet.load(modelPath);
+    await faceapi.nets.faceExpressionNet.load(modelPath);
+    optionsSSDMobileNet = new faceapi.SsdMobilenetv1Options({ minConfidence: minScore, maxResults });
+  }
+
+  async function main() {
+    await faceapi.tf.setBackend('webgl');
+    await faceapi.tf.ready();
+
+    if (faceapi.tf?.env().flagRegistry.CANVAS2D_WILL_READ_FREQUENTLY) faceapi.tf.env().set('CANVAS2D_WILL_READ_FREQUENTLY', true);
+    if (faceapi.tf?.env().flagRegistry.WEBGL_EXP_CONV) faceapi.tf.env().set('WEBGL_EXP_CONV', true);
+    if (faceapi.tf?.env().flagRegistry.WEBGL_EXP_CONV) faceapi.tf.env().set('WEBGL_EXP_CONV', true);
+
+    await setupFaceAPI();
+    await setupCamera();
+  }
+
   return (
-    <>
-      <Container className='webcam-main-container'>
+      <Container className='webcam-main-container' style={borderStyle}>
         <div className='webcam-container'>
           <video id="video" playsInline className="video" ref={videoRef}></video>
           <canvas id="canvas" className="canvas" ref={canvasRef}></canvas>
         </div>
+        <a href="#login-component">back</a>
       </Container>
-    </>
   );
 }
 
