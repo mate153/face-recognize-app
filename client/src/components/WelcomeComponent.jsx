339082806af44424b9bcf631e80c74734156d867
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2024-02-12 17:51:35 +0100

    [ UPDATE ] User feedback secton added

--- client/src/components/WelcomeComponent.jsx
+++ client/src/components/WelcomeComponent.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import Swal from 'sweetalert2';
 
 function WelcomeComponent() {
+  useEffect(() => {
+    setTimeout(() => {
+      Swal.fire({
+        title: "Please rate the process!",
+        icon: "question",
+        input: "range",
+        inputLabel: "How satisfied are you with the login?",
+        inputAttributes: {
+          min: "0",
+          max: "10",
+          step: "1"
+        },
+        inputValue: 5
+      });
+    }, 4000);
+  }, []);  
   return (
     <>
       <div style={{color: 'white'}}>Welcome</div>
     </>
-  )
-}
+  );
+};
 
 export default WelcomeComponent
